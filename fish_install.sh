303a5402e96316125b5bbee8180f7e4175ea16bf
kotIIT <mkotlyar@hawk.iit.edu>
2020-03-31 01:09:59 -0500

    fixed tabbing

--- fish_install.sh
+++ fish_install.sh
@@ -2,28 +2,28 @@
 #install and config fish shell
 
 install_config(){
-	sudo apt-add-repository ppa:fish-shell/release-3
-	sudo apt update
-	sudo apt install fish -y
-	curl -L https://get.oh-my.fish | fish
+  sudo apt-add-repository ppa:fish-shell/release-3
+  sudo apt update
+  sudo apt install fish -y
+  curl -L https://get.oh-my.fish | fish
 }
 
 change_env(){
-	echo /usr/bin/fish | sudo tee -a /etc/shells
-	chsh -s /usr/bin/fish
+  echo /usr/bin/fish | sudo tee -a /etc/shells
+  chsh -s /usr/bin/fish
 }
 
 get_omf(){
-	./omf_tweaks.fish
+  ./omf_tweaks.fish
 }
 
 easy_setup(){
   install_config
-	change_env
+  change_env
 }
 
 missing(){
-	sudo apt-get install build-essential cmake ncurses-dev libncurses5-dev libpcre2-dev gettex
+  sudo apt-get install build-essential cmake ncurses-dev libncurses5-dev libpcre2-dev gettex
 }
 
 arg=$1
